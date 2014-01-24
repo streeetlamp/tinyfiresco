24d0859928266869a7953f50887da75415ae12d0
Cody Whitby <cody@codywhitby.net>
2014-01-23 20:08:21 -0500

    day,night/SVG images

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+var today = new Date().getHours();
+
+if (today >= 7 && today <= 19) {
+   $('body').addClass('day');
+} else {
+   $('body').addClass('night');
+}
+
+})(jQuery, window, document);
 
